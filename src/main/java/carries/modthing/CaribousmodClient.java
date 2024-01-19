2513d0930f7a450f02862520ca4add50133a3305
BuildTools <unconfigured@null.spigotmc.org>
2024-01-19 19:46:09 +0000

    First commit ig?

---
+++ src/main/java/carries/modthing/CaribousmodClient.java
@@ -0,0 +1,10 @@
+package carries.modthing;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CaribousmodClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
